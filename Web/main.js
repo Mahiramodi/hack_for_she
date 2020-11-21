656e7e51d31693552946c92cab26d8906673fc17
Kunal Khullar <kunalkhullar5012@gmail.com>
2020-11-21 20:40:47 +0530

    bug fixes

--- Web/main.js
+++ Web/main.js
@@ -1,5 +1,6 @@
-var i=1;
+var i=1,j=0,k=0;
 var address = [];
+var lat=0,lng=0;
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
  fetch(proxyurl + 'https://raise-backend.herokuapp.com/graphql',  {
     method: 'POST',
@@ -11,6 +12,7 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
           police{
             latitude
             longitude
+            time
             user {
               profile {
                 name
@@ -24,18 +26,35 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
         `
     })
 }).then(res => res.json())
-.then(async data =>  {
-    await data.data.police.forEach(async police => {
+.then( data =>  {
+  console.log(data.data.police)
+  
+     data.data.police.forEach(async police => {
+      lat=police.latitude
+      lng=police.longitude
+      console.log(lat)
+      var mob = police.user.profile.mobile;
+      var pro = police.user.profile.name;
+      var time = police.time;
+      console.log(k)
+      k++;
       
+      var data1 = "";
+      await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=efe8f8cf9ec6408f8064758d8fb335d9`)
+      .then(res => res.json())
+      .then(data =>{
+        // console.log(data.results[0].formatted)
+        // address.push(data.results[0].formatted)
+        data1=data.results[0].formatted;
+      })
+      console.log(data1,lat)
+      document.getElementById("tableContent").innerHTML += `<tr><th scope='row'>${i}</th><td>${pro}</td>
+      <td>${mob}</td><td>${data1}</td><td>${police.latitude}</td><td>${police.longitude}</td><td>${time}</td></tr>`
       
-   await fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.d22f1d115a3cfee16e28fa3ff2d8e5e6&lat=${police.latitude}&lon=${police.longitude}&format=json`)
-    .then(res => res.json())
-    .then(data => address.push(data.display_name))
-    console.log(address);
-      document.getElementById("tableContent").innerHTML += `<tr><th scope='row'>${i}</th><td>${police.user.profile.name}</td>
-      <td>${police.user.profile.mobile}</td><td>${address[i-1]}</td><td>${police.latitude}</td><td>${police.longitude}</td><td>${"2:00 PM"}</td></tr>`
       i++;
+      j++;
     })
+    console.log(address);
 })
 
 
